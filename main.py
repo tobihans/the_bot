401763063b8e11567ed9b28fd864ea21d26646f9
Hans B. K. Tognon <git@tobihans.space>
2025-02-18 13:27:25 +0100

    feat: take user input into account

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ import gradio as gr
 
 
 def random_response(message, history):
+    print(message)
+    __import__("pprint").pprint(history)
     return random.choice(["Yes", "No"])
 
 
+def agree_disagree(message, history):
+    if len([h for h in history if h["role"] == "assistant"]) % 2 == 0:
+        return f"Yes, I do think that: {message}"
+    else:
+        return "No, I do not think that."
+
+
 def main():
-    gr.ChatInterface(fn=random_response, type="messages").launch(debug=True)
+    gr.ChatInterface(fn=agree_disagree, type="messages").launch(debug=True)
 
 
 if __name__ == "__main__":
